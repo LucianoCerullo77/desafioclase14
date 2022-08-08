fac95b036f46adc1b52db1d090b86f39f638250f
Luciano Cerullo <lucianonicolas10@gmail.com>
2022-08-08 13:04:20 -0300

    server y productos del JSON

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.use('/api/carrito', routerCart)
 routerProducts.use(express.json())
 routerCart.use(express.json())
 
-const products = new ClaseProductos('products.json')
-const cart = new ClaseCarritos('cartProducts.json')
+const products = new ClassProducts('products.json')
+const cart = new ClassCart('cartProducts.json')
  
 function adminAccess(req, res, next) {
     if (req.body.admin === Boolean(true)) {
@@ -29,4 +29,60 @@ function adminAccess(req, res, next) {
     }
 }
 
+routerProducts.get('/:id?', (req, res) => {
+    const id = req.params.id
+    if (id) {
+        res.send(products.getProduct(id))
+    } else {
+        res.send(products.getProducts())
+    }
+})
 
+routerProducts.post('/', adminAccess, (req, res) => {
+    const product = products.createProduct(req.body)
+    res.send(product)
+})
+
+routerProducts.put('/:id', adminAccess, (req, res) => {
+    const id = req.params.id
+    const product = products.updateProduct(id, req.body)
+    res.send(product)
+})
+
+routerProducts.delete('/:id', adminAccess, (req, res) => {
+    const id = req.params.id
+    const product = products.deleteProduct(id)
+    res.send(product)
+})
+
+routerCart.get('/:id/productos', (req, res) => {
+    const id = req.params.id
+    const cart = cart.getCartProducts(id)
+    res.send(cart)
+})
+
+routerCart.post('/:id/productos', (req, res) => {
+    const id = req.params.id
+    const cart = cart.addProduct(id, req.body)
+    res.send(cart)
+})
+
+routerCart.delete('/:id/productos/:idProducto', (req, res) => {
+    const id = req.params.id
+    const idProducto = req.params.idProducto
+    const cart = cart.deleteProduct(id, idProducto)
+    res.send(cart)
+})
+
+app.get('*', (req, res) => {
+    res.send({
+        error: -2,
+        descripcion: req.protocol + '://' + req.get('host') + req.originalUrl,
+        método: req.method,
+        status: 'error : Ruta inexistente'
+    })
+})
+
+app.listen(8080, () => {
+    console.log('Servidor iniciado en el puerto 8080')
+})

d2e96af80639487ff0119ea7ebc0e19542fc743e
Luciano Cerullo <lucianonicolas10@gmail.com>
2022-08-08 12:17:30 -0300

    update creado

--- products.js
+++ products.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 
+const date = new Date()
 module.exports = class Products {
     constructor(file) {
         this.file = file
@@ -13,5 +14,33 @@ module.exports = class Products {
     }
     else {
         id = this.products[this.products.length - 1].id + 1
-    }}
+    }
+
+    let timestamp = date.getDate() + '-' + date.getMonth() + '-' + date.getFullYear() + ' ' + date.getHours() + ':' + date.getMinutes()
+    const product = {
+        id: id,
+        timestamp: timestamp,
+        ...obj
+    }
+    this.products.push(product)
+    fs.writeFileSync(this.file, JSON.stringify(this.products))
+    return product
+}
+
+    update(id, obj) {
+        const index = this.products.findIndex(product => product.id === id)
+        if (index === -1) {
+            return ({error: -1, descripcion: 'No se encontró el producto'})
+        }
+        else {
+            this.products[index].nombre = obj.nombre || this.products[index].nombre
+            this.products[index].precio = obj.precio || this.products[index].precio 
+            this.products[index].descripcion = obj.descripcion || this.products[index].descripcion
+            this.products[index].fotoURL = obj.fotoURL || this.products[index].fotoURL
+            this.products[index].stock = obj.stock || this.products[index].stock
+
+            fs.writeFileSync(this.file, JSON.stringify(this.products))
+            return('Producto actualizado')
+        }
+    }
 }
